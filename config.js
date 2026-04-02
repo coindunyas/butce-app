94eb374dd4cc277133b541902b5e72d7de8afc8a
ercan <tommylife1996@gmail.com>
2026-04-02 13:25:15 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
 window.BUTCE_API_BASE = "https://butce-backend-9bbd.onrender.com";
-// Backend'i Render/Railway/VPS'e kurduğunda burayı örneğin:
-// window.BUTCE_API_BASE = "https://senin-backend-adresin.com";
