01e0e04145dfdbd5e3ef9f7bce2986ffdb04d699
ercan <tommylife1996@gmail.com>
2026-04-02 12:14:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-window.BUTCE_API_BASE = "http://localhost:3000";
+window.BUTCE_API_BASE = "https://butce-backend-9bbd.onrender.com";
 // Backend'i Render/Railway/VPS'e kurduğunda burayı örneğin:
 // window.BUTCE_API_BASE = "https://senin-backend-adresin.com";
